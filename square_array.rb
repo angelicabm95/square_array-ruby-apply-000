6ba67c51e027bfad2bbb78359954f35ed9393ed9
Angelica Burgos Mendez <angelicabm95@gmail.com>
2018-07-12 18:43:17 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 numbers = [1,2,3]
 
-def square_array(numbers)
-  new_num = []
-  numbers.each { |x| new_num << (x**2) }
-  return new_num
+def square_array(nums)
+  new_nums = []
+  nums.each { |x| new_nums << (x**2) }
+  return new_nums
 end
